a218212e200f5f4d53127087b4229864112d1a98
Andrés García <andres.ggarcia@mercadolibre.com>
2019-02-25 20:15:55 -0300

    Fixed eliminar + Eslint warnings

--- src/app/pages/todo-details/todo-details.page.ts
+++ src/app/pages/todo-details/todo-details.page.ts
@@ -2,51 +2,56 @@ import { Todo, TodoService } from './../../services/todo.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavController, LoadingController } from '@ionic/angular';
- 
+
 @Component({
   selector: 'app-todo-details',
   templateUrl: './todo-details.page.html',
   styleUrls: ['./todo-details.page.scss'],
 })
 export class TodoDetailsPage implements OnInit {
- 
+
   todo: Todo = {
     titulo: 'Prueba',
     descripcion: 'Prueba descripcion',
     fecha: new Date().getTime(),
     gradoId: '07_'
   };
- 
+
   todoId = null;
- 
-  constructor(private route: ActivatedRoute, private nav: NavController, private todoService: TodoService, private loadingController: LoadingController) { }
- 
+
+  constructor(
+    private route: ActivatedRoute,
+    private nav: NavController,
+    private todoService: TodoService,
+    private loadingController: LoadingController
+  ) { }
+
   ngOnInit() {
     this.todoId = this.route.snapshot.params['id'];
-    if (this.todoId)  {
+    if (this.todoId) {
       this.loadTodo();
     }
   }
- 
+
   async loadTodo() {
     const loading = await this.loadingController.create({
-      message: 'Loading Todo..'
+      message: 'Cargando..'
     });
     await loading.present();
- 
+
     this.todoService.getTodo(this.todoId).subscribe(res => {
       loading.dismiss();
       this.todo = res;
     });
   }
- 
+
   async saveTodo() {
- 
+
     const loading = await this.loadingController.create({
-      message: 'Saving Todo..'
+      message: 'Guardando..'
     });
     await loading.present();
- 
+
     if (this.todoId) {
       this.todoService.updateTodo(this.todo, this.todoId).then(() => {
         loading.dismiss();
@@ -59,5 +64,5 @@ export class TodoDetailsPage implements OnInit {
       });
     }
   }
- 
+
 }
